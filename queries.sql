5d2457997235229e8487f8cae2a15b1b18e3c891
Tomas Ortiz <TomasOrtiz@protonmail.com>
2020-02-19 19:26:29 -0800

    Queries finished

--- queries.sql
+++ queries.sql
@@ -5,17 +5,31 @@
 SELECT CategoryName, ProductName 
 FROM Product 
 JOIN Category 
-ON Product.CategoryId = Category.id
+ON Product.CategoryId = Category.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 SELECT [Order].id, CompanyName
 FROM [Order]
-JOIN Shipper = [Order]
+JOIN Shipper 
+ON [Order].ShipVia = Shipper.id
+WHERE [Order].OrderDate < '2012-08-09';
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT ProductName, Quantity
+FROM Product
+JOIN OrderDetail
+ON Product.id = OrderDetail.ProductId
+WHERE OrderDetail.OrderId = '10251'
+ORDER BY ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
+SELECT [Order].Id, CompanyName, LastName as EmployeeLastName
+FROM [Order]
+JOIN Employee 
+ON Employee.Id = [Order].EmployeeId
+JOIN Customer
+ON Customer.Id = [Order].CustomerId;
